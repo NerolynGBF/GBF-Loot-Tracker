352322f5cf8711a4230078dd23fd4da348d1e068
Joshua Alford <143864162+NerolynGBF@users.noreply.github.com>
2023-09-04 23:43:40 -0400

    Add loot by raid, blue chest tracking, raid count tracking, frontend libraries

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,19 +1,94 @@
 function clearRowsFromTable(table_id) {
     let table = document.getElementById(table_id);
-    while (table.rows.length > 1) {
-      table.deleteRow(1);
+    while (table.rows.length > 0) {
+      table.deleteRow(0);
     }
 }
+
+function simplify(str) {
+    let result = '', data = str.split('/'),
+        numOne = Number(data[0]),
+        numTwo = Number(data[1]);
+    for (let i = Math.max(numOne, numTwo); i > 1; i--) {
+    if ((numOne % i == 0) && (numTwo % i == 0)) {
+        numOne /= i;
+        numTwo /= i;
+    }
+    }
+    if (numTwo === 1) {
+    result = numOne.toString()
+    } else {
+    result = numOne.toString() + '/' + numTwo.toString()
+    }
+    return result
+}
   
-function addRowsToTable(table_id, array) {
-    let table = document.getElementById(table_id);
-    array.forEach((item) => {
-        let row = table.insertRow();
+function addRowsToTable(table, array, options = {}) {
+    let t = document.getElementById(table) || table;
+    let table_id = t.id;
+    array.forEach((item, i) => {
+        let row = t.insertRow();
+        if(options['row-ids']) {
+            row.id = options['row-ids'][i];
+        }
+        if (options['row-classes']) {
+            options['row-classes'].forEach((row_class) => {
+                row.classList.add(row_class);
+            });
+        }
+        if (options['row-attributes']) {
+            for (let [attribute, value] of options['row-attributes']) {
+                row.setAttribute(attribute, value);
+            }
+        }
+        if (options['expandable-rows']) {
+            row.setAttribute("data-bs-toggle", "collapse");
+            row.setAttribute("data-bs-target", "." + table_id + "-" + options['row-ids'][i]);
+        }
         item.forEach((value) => {
             let cell = row.insertCell();
             cell.innerHTML = value;
+            if (options['cell-classes']) {
+                options['cell-classes'].forEach((cell_class) => {
+                    cell.classList.add(cell_class);
+                });
+            }
         });
     });
+}
+
+function createTable(table_id, table_header, table_data, options = {}) {
+    let new_table = document.createElement('table');
+    new_table.id = table_id;
+    if (options['table-classes']) {
+        options['table-classes'].forEach((table_class) => {
+            new_table.classList.add(table_class);
+        });
+    }
+    let header = new_table.createTHead();
+    let header_row = header.insertRow();
+    let body = new_table.createTBody();
+    let body_row = body.insertRow();
+    table_header.forEach((item) => {
+        let cell = header_row.insertCell();
+        cell.innerHTML = item;
+    });
+    if (options['div-wrapper']) {
+        let new_div = document.createElement('div');
+        new_div.id = table_id + "-div";
+        if (options['div-classes']) {
+            options['div-classes'].forEach((div_class) => {
+                new_div.classList.add(div_class);
+            });
+        }
+        new_div.appendChild(new_table);
+        addRowsToTable(new_table, table_data, options);
+        return new_div;
+    }
+    else {
+        addRowsToTable(new_table, table_data, options);
+        return new_table;
+    }
 }
 
 function setStorage(key, value) {
@@ -36,4 +111,4 @@ const clearStorage = async () => {
     chrome.storage.local.clear();
 }
 
-export { clearRowsFromTable, addRowsToTable, setStorage, getStorage, clearStorage };
+export { clearRowsFromTable, addRowsToTable, setStorage, getStorage, clearStorage, createTable, simplify };
--- src/js/network.js
+++ src/js/network.js
@@ -3,36 +3,87 @@ import {
   addRowsToTable,
   setStorage,
   getStorage,
-  clearStorage,
+  createTable,
+  simplify
 } from "./helpers.js";
 
 async function main() {
   let tab_id = parseInt(window.location.search.substring(1));
   let rewards = await getStorage("rewards");
+  let raid_history = await getStorage("raid_history");
+  let raid_dict = await getStorage("raid_dict");
   clearRowsFromTable("loot-table");
   addRowsToTable("loot-table", allRewardsToRow(rewards));
+  buildRaidTable(raid_history);
   chrome.debugger.attach({ tabId: tab_id }, "1.0", function (e) {
     chrome.debugger.sendCommand({ tabId: tab_id }, "Network.enable");
-
     chrome.debugger.onEvent.addListener(function (source, method, params) {
       if (method === "Network.responseReceived") {
         let url = params.response.url;
-
-        if (url.includes("resultmulti/data") || url.includes("result/data")) {
+        let new_rewards = [];
+        let name = "";
+        let blue_chested = false;
+        if (url.includes("rest/multiraid/start.json")) {
           chrome.debugger.sendCommand(
             { tabId: source.tabId },
             "Network.getResponseBody",
             { requestId: params.requestId },
             function (response) {
-              let list = JSON.parse(response.body).rewards.reward_list;
-              for (let [_key, value] of Object.entries(list)) {
+              let twitter = JSON.parse(response.body)["twitter"];
+              raid_dict[twitter["raid_id"]] = { name: twitter["monster"], raid_id: twitter["raid_id"], enemy_id: twitter["enemy_id"] };
+              setStorage("raid_dict", raid_dict)
+            });
+        }
+        else if (url.includes("resultmulti/data") || url.includes("result/data")) {
+          const regex = /(?!\/)\d+(?=\?)/gm;
+          let raid_id = url.match(regex)[0];
+          chrome.debugger.sendCommand(
+            { tabId: source.tabId },
+            "Network.getResponseBody",
+            { requestId: params.requestId },
+            function (response) {
+              let body = JSON.parse(response.body);
+              let list = body.rewards.reward_list;
+              
+              if (body["retry_quest_info"]) {
+                name = body["retry_quest_info"]["chapter_name"];
+                raid_dict[raid_id] = { name: body["retry_quest_info"]["chapter_name"], raid_id: raid_id, enemy_id: body["retry_quest_info"]["quest_id"] };
+                setStorage("raid_dict", raid_dict)
+              }
+              else if (raid_dict[raid_id]) {
+                name = raid_dict[raid_id].name;
+              }
+
+              for (let [key, value] of Object.entries(list)) {
+                if (key == "11" && !Array.isArray(value)) {
+                  blue_chested = true;
+                }
                 for (let [_k, v] of Object.entries(value)) {
-                  rewards = addReward(rewards, parseReward(v));
+                  new_rewards.push(parseReward(v));
                 }
               }
+              
+              rewards = addRewards(rewards, new_rewards);
               clearRowsFromTable("loot-table");
               addRowsToTable("loot-table", allRewardsToRow(rewards));
               setStorage("rewards", rewards);
+              if(name!="") {
+                if (!raid_history[name]) {
+                  raid_history[name] = {};
+                  raid_history[name]['rewards'] = {};
+                  raid_history[name]['kill_count'] = 0;
+                  raid_history[name]['blue_chest_count'] = 0;
+                  raid_history[name]['enemy_id'] = raid_dict[raid_id]['enemy_id'];
+                }
+                raid_history[name]['rewards'] = addRewards(raid_history[name]['rewards'], new_rewards);
+                raid_history[name]['kill_count']+=1;
+                if ( blue_chested ) { raid_history[name]['blue_chest_count']+=1 };
+                clearRowsFromTable("raid-table");
+                buildRaidTable(raid_history);
+                delete raid_dict[raid_id];
+                setStorage("raid_history", raid_history);
+                setStorage("raid_dict", raid_dict)
+              }
             }
           );
         }
@@ -44,33 +95,69 @@ async function main() {
 function parseReward(reward) {
   let result = {};
   result["name"] = reward["name"];
-  result["count"] = reward["count"];
+  result["count"] = parseInt(reward["count"]);
   result["id"] = reward["id"];
   result["rarity"] = reward["rarity"];
   result["type"] = reward["type"];
   return result;
 }
 
-function addReward(rewards, new_reward) {
+function buildRaidTable(raid_history){
+  addRowsToTable("raid-table", allRaidsToRow(raid_history), { "expandable-rows": true, "row-classes": ['accordion-toggle'], "row-ids": Object.values(raid_history).map((x) => x['enemy_id']) });
+  for (let [key, value] of Object.entries(raid_history)) {
+    let raid_table = document.getElementById("raid-table");
+    let table = createTable(
+      "raid-table-" + value['enemy_id'],
+      ["Image", "Name", "Count", "Per Blue Chest", "Per Raid"],
+      allRewardsToRow(value['rewards'], { "include-stats": true, "raid-count": value['kill_count'], "blue-chest-count": value['blue_chest_count'] }),
+      { 
+        "table-classes": ["w-100", "table-sm"],
+        "div-wrapper": true,
+        "div-classes": ["accordion", "collapse", "raid-table-" + value['enemy_id']],
+        "row-classes": ["raid-table-" + value['enemy_id']],
+      }
+    );
+    let new_tr = raid_table.insertRow(document.getElementById(value['enemy_id']).rowIndex);
+    new_tr.classList.add("raid-table-" + value['enemy_id'], "collapse");
+    let new_td = new_tr.insertCell()
+    new_td.setAttribute("colspan", "3");
+    new_td.appendChild(table);
+  };
+}
+
+function addRewards(rewards, new_rewards) {
   let result = {};
-  let id = new_reward["id"];
-  result = JSON.parse(JSON.stringify(rewards));
-  result[id] = new_reward;
-  if (rewards[id]) {
-    result[id]["count"] = (parseInt(rewards[id]["count"]) || 0) + parseInt(new_reward["count"]);
-  }
+  let id = "";
+  result = JSON.parse(JSON.stringify(rewards || {}));
+  new_rewards.forEach((reward) => {
+    id = reward["id"];
+    if (result[id]) {
+      result[id]["count"] = (parseInt(result[id]["count"]) || 0) + parseInt(reward["count"]);
+    }
+    else {
+      result[id] = reward;
+    }
+  });
   return result;
 }
 
-function allRewardsToRow(rewards) {
+function allRewardsToRow(rewards, options = {}) {
   let result = [];
   for (let [_key, value] of Object.entries(rewards)) {
-    result.push(rewardToRow(value));
+    result.push(rewardToRow(value, options));
   }
   return result;
 }
 
-function rewardToRow(reward) {
+function allRaidsToRow(raids) {
+  let result = [];
+  for (let [key, value] of Object.entries(raids)) {
+    result.push(raidToRow(key, value));
+  }
+  return result;
+}
+
+function rewardToRow(reward, options = {}) {
   let result = [];
   result.push(
     "<img src='https://prd-game-a1-granbluefantasy.akamaized.net/assets_en/img/sp/assets/" +
@@ -81,6 +168,18 @@ function rewardToRow(reward) {
   );
   result.push(reward["name"]);
   result.push(reward["count"]);
+  if (options["include-stats"]) {
+    result.push((options["blue-chest-count"] > 0) ? simplify((reward["count"]+"/"+options["blue-chest-count"])) : "N/A")
+    result.push((options["raid-count"] > 0) ? simplify((reward["count"]+"/"+options["raid-count"])) : "N/A")
+  }
+  return result;
+}
+
+function raidToRow(name, data) {
+  let result = [];
+  result.push(name);
+  result.push(data["blue_chest_count"]);
+  result.push(data["kill_count"]);
   return result;
 }
 
